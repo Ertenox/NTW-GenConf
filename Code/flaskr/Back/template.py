946b192face830f5d2045626340a52ab0786307e
Ertenox <auguste.mendy@etu.imt-nord-europe.fr>
2024-11-25 17:47:11 +0100

    Code buge

--- Code/flaskr/Back/template.py
+++ Code/flaskr/Back/template.py
@@ -2,25 +2,25 @@ import re
 
 class Template:
 
-    def __init__(self, title, content = "", dict = {}):
+    def __init__(self, title, content = "", dict = {}, imported = False):
         self.title = title.split('\\')[-1]
         self.content = content
+        self.imported = imported
         self.__dict = dict
         self.__outContent = ""
+        print("appelle de __init__ pour le template", self.title, "les variables sont", self.__dict)
 
     def findAllVar(self):
         temp = re.findall(r"\$\{([^}]+)\}", self.content)
         temp.sort()
+        print("appelle de findAllVar pour le template", self.title, "les variables sont", temp)
         for var in temp:
             if var != "_MODE_CLI_":
                 self.__dict[var] = ""
         
     
-    def getAllVar(self) -> dict:
-        self.findAllVar()
-        return self.__dict 
-    
     def setVar(self, var, value):
+        print("appelle de setVar pour le template", self.title, "la variable est", var, "la valeur est", value)
         if var in self.__dict:
             self.__dict[var] = value
         else:
@@ -34,10 +34,13 @@ class Template:
                 self.__outContent = re.sub(r"\$\{"+i+r"\}", self.__dict[i], self.__outContent)
 
     def getDict(self) -> dict:
+        print("appelle de getDict pour ", self.title, "les variables sont", self.__dict)    
         return self.__dict
 
     def getOutContent(self) -> str:
         return self.__outContent
+    
+  
     
 
     
